746d8fabd31d534b8c6e15da11ef6aa32eb9d172
William <william.gispiela@gmail.com>
2023-10-22 21:12:28 -0300

    first commit. Estrutura básica feita

---
+++ main.js
@@ -0,0 +1,6 @@
+// 1 - Coletar o valor do textarea
+// 2 - Identificar o valor do input de cifragem ou descifragem
+// 3 - Ao clicar no botão, realizar lógica de cifragem
+// 4 - Retornar e substituir o valor final para o textarea
+
+const letters = []
