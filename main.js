e7b0d6698550842c162ee6ea75b64ecafab09680
William <william.gispiela@gmail.com>
2023-10-23 21:29:16 -0300

    parte logica completa com cifrador e descifrador operacionais

--- main.js
+++ main.js
@@ -1,48 +1,81 @@
-// Lógica inicial:
-// 1 - Coletar o valor do textarea
-// 2 - Identificar o valor do input de cifragem ou descifragem
-// 3 - Ao clicar no botão, realizar lógica de cifragem
-// 4 - Retornar e substituir o valor final para o textarea
-
-// Considerações:
-// Na tabela ASCII: A = 65
-// A lógica deverá ser de capturar o valor do caractere, somar o valor de cifragem que seria o descloamento e % do valor máximo
-
-const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
 let cifrarBtn = document.getElementById('idCifrarBtn');
-cifrarBtn.addEventListener('click', function(event) {
+cifrarBtn.addEventListener('click', function() {
 
+    // Mapear as letras do alfabeto para seus valores numéricos
+    const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alfabetoArray = alfabeto.split(''); // .split para cada caractere da string 'alfabeto' estar em um array
+
+    // Adquirindo campos da tela
     let texto = document.getElementById("idText").value.toUpperCase();
-    let cifragem = parseInt(document.getElementById('idCifra').value);
+    let cifragem = document.getElementById('idCifra').value; 
+    // como cifragem pode ser numero ou letra, não será adicionado parseInt ou Number
+
+
+    // Se a cifragem for uma letra, converte para número
+    if (/^[a-zA-Z]$/.test(cifragem)) {
+        cifragem = alfabetoArray.indexOf(cifragem.toUpperCase()) + 1; 
+        // percorre o array e adiciona +1 ao indice da letra, visto que a = 0 antes disso, e queremos a = 1
+    } else {
+        cifragem = parseInt(cifragem);
+    }
+
     let resultado = '';
 
-    //Adicionando uma condição para converter um caractere alfabético para um valor numérico.
-    if (/[A-Z]/.test(cifragem)) {
-        cifragem = cifragem.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0) + 1;
-    } else if (/[0-9]/.test(cifragem)) {
-        cifragem = parseInt(cifragem);
-    } else {
-        alert("A cifra deve ser um número ou um caractere alfabético.");
-    }
-
     for (let i = 0; i < texto.length; i++) {
-        //Adquirindo o valor de indice de cada letra do texto
-        let letra = texto.charCodeAt(i) - 65;
+        // Declarando uma variável para guardar o valor de acordo com a tabela ASCII
+        let textoASCII = texto.charCodeAt(i);
 
-        //Identificando se no texto há um espaço vazio ou ç
-        if (letra === ' ') {
-            resultado = `${resultado} `;  
-        } 
-        else if (letra === 'ç') {
-            resultado = `${resultado}ç`;
-        }
-        // Agora no else, após identificarmos a possibilidade de espaço vazio e ç, realizamos a lógica principal 
-        else {
-                let letraComDeslocamento = (letra + cifragem) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
-                letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; //MOD retornar 0 caso o resultado seja 26, tem que tratar isso
-                resultado += letras[letraComDeslocamento]; 
+        if (textoASCII < 65 || textoASCII > 90) { // Necessário prever os casos que o que foi digitado é ' ' ou 'ç', a qual nesse caso retornará o próprio caractere
+            resultado += texto[i] // 
+        } else if (textoASCII >= 65 && textoASCII <= 90) { //Na tabela ASCII, entre 65 e 90 que está contido o alfabeto em letras maiusculas
+            let novoTexto = textoASCII + cifragem;
+                if (novoTexto > 90) {
+                novoTexto = novoTexto - 26; // Para reiniciar a contagem em A após Z
+                }
+            resultado += String.fromCharCode(novoTexto);
+        } else {
+            resultado += String.fromCharCode(textoASCII);
         }
     }
-    document.getElementById("idOut").value = resultado;
-});
+    document.getElementById("idOutCifrado").innerText = resultado;
+
+})
+
+
+let descifrarBtn = document.getElementById('idDescifrarBtn');
+descifrarBtn.addEventListener('click', function() {
+
+        const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const alfabetoArray = alfabeto.split(''); 
+    
+        let texto = document.getElementById("idText").value.toUpperCase();
+        let cifragem = document.getElementById('idCifra').value; 
+    
+    
+        if (/^[a-zA-Z]$/.test(cifragem)) {
+            cifragem = alfabetoArray.indexOf(cifragem.toUpperCase()) + 1; 
+        } else {
+            cifragem = parseInt(cifragem);
+        }
+    
+        let resultado = '';
+    
+        for (let i = 0; i < texto.length; i++) {
+
+            let textoASCII = texto.charCodeAt(i);
+    
+            if (textoASCII < 65 || textoASCII > 90) { 
+                resultado += texto[i] 
+            } else if (textoASCII >= 65 && textoASCII <= 90) { 
+                let novoTexto = textoASCII - cifragem; // Como é para descifrar, subtraimos aqui
+                    if (novoTexto < 65) { // Invés de 90, o valor aqui é 65, e novoTexto MENOR que 65.
+                    novoTexto = novoTexto + 26; // Somamos 26. invés de subtrair
+                    }
+                resultado += String.fromCharCode(novoTexto);
+            } else {
+                resultado += String.fromCharCode(textoASCII);
+            }
+        }
+        document.getElementById("idOutDescifrado").innerText = resultado;
+
+})
